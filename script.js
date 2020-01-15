86ebd0c5c8ce17edf943dc7547a23452f4aae22f
kpajak <59928323+kpajak@users.noreply.github.com>
2020-01-15 18:28:14 +0100

    zrobione zad 2 zaj 2

--- script.js
+++ script.js
@@ -3,23 +3,8 @@ function(Map, MapView){
     let map1 = new Map({basemap:"topo"}); //słowo klucz new + nazwa modułu
     let mapContainer = new MapView({
         container: "mapa",  // miejsce gdzie chcemy osadzić mapę (bierzemy klasę z pliku html //
-        map: map1  //czyli nasza zmienna z mapą bazową zdefiniowana wcześniej //
-        
+        map: map1,  //czyli nasza zmienna z mapą bazową zdefiniowana wcześniej //
+        zoom: 12
     });
     
 });
-/*require(["esri/widgets/Zoom"], function(Zoom) {
-    let zoom = new Zoom({
-        container: "mapa"
-        view: view;
-    });
-
-});
-
-require(["esri/widgets/NavigationToggle"], 
-function(NavigationToggle) { 
-    let navigationToggle = new NavigationToggle({
-        view: view;
-        container: "mapa"
-      });
- });*/
